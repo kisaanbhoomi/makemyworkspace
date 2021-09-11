98a049aadeb1b2110c5918918ea1bac13e6bc85a
Sanjay Shiradwade <sanjayshiradwade@gmail.com>
2021-09-11 20:59:31 +0530

    go boilerplate

---
+++ Go_workspace/README.md
@@ -0,0 +1,7 @@
+### Create Go App
+
+ > creategoapp.sh will create an go boilerpalte 
+
+Run
+ 	
+	sh creategoapp.sh -g github_username -p project_name
---
+++ Go_workspace/creategoapp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+CREATEAPP=true
+
+while getopts "g:p:" input; do
+	case "${input}" in
+	g)
+		GITHUBUSERNAME=${OPTARG}
+		;;
+	p)
+		PROJECT=${OPTARG}
+		;;
+	esac
+done
+
+usage() { echo "[-g github user name] [-p project name]"; }
+
+if [ -z "$GITHUBUSERNAME" ] || [ -z "$PROJECT" ]; then
+	CREATEAPP=false
+	usage
+fi
+
+if [ "$CREATEAPP" == true ]; then
+	mkdir KisanBhoomi && cd KisanBhoomi && mkdir -p {bin,github.com,pkg} && cd github.com && mkdir "${GITHUBUSERNAME}" && cd "${GITHUBUSERNAME}" && mkdir "${PROJECT}" && cd "${PROJECT}" && mkdir src && touch main.go && go mod init github.com/"${GITHUBUSERNAME}"/"${PROJECT}" && cd src && mkdir -p {controller,middleware,response} && touch server.go
+fi
